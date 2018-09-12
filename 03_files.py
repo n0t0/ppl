969647314b9c4421a665cc4ded8d5a4b5ceddcc7
Ivo <vn.n0t0@gmail.com>
2018-09-12 16:02:15 -0700

    file - update

---
+++ 03_files.py
@@ -0,0 +1,13 @@
+f = open('schedule.txt', 'r')
+# print(f)
+
+# data = f.read()
+# print(data)
+
+# for line in f:
+#     print(line)
+
+# f.close()
+
+with open('schedule.txt', 'r') as f:
+    data = f.read()
