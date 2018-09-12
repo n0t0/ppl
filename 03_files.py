6620a11a4f351fe9cf342b8d893102595ac9080d
Ivo <vn.n0t0@gmail.com>
2018-09-12 16:28:22 -0700

    file - update

--- 03_files.py
+++ 03_files.py
@@ -1,13 +1,28 @@
-f = open('schedule.txt', 'r')
-# print(f)
+# f = open('schedule.txt', 'r')
+# # print(f)
 
-# data = f.read()
-# print(data)
+# # data = f.read()
+# # print(data)
 
 # for line in f:
 #     print(line)
 
-# f.close()
+# # f.close()
 
-with open('schedule.txt', 'r') as f:
-    data = f.read()
+# # with open('schedule.txt', 'r') as f:
+# #     data = f.read()
+
+# total = 0.0 
+
+with open('data.csv', 'r') as f:
+    headers = next(f)       # Skip a single line of input 
+    for line in f: 
+        line = line.strip()
+        parts = line.split(',')
+        # parts[0] = parts[0].strip('"')
+        # parts[1] = parts[1].strip('"')
+        total += parts[4]*parts[5]
+        # print(parts)
+        
+        
+print('Total cost:', total)
