85d9e42bf9947b770560818ad2ecc79d5bb84470
Ivo <vn.n0t0@gmail.com>
2018-09-13 16:20:25 -0700

    libraries and functions import

---
+++ 06_lib_func_import.py
@@ -0,0 +1,8 @@
+import simple
+
+print(simple.x)
+
+import sys
+print(sys.modules['simple']) # module stored in python's cache
+
+print(sys.path)
---
+++ simple.py
@@ -0,0 +1,13 @@
+x = 42
+
+def spam():
+    print('x is:', x)
+
+
+def run():
+    print('Calling spam')
+    spam()
+
+if __name__ == '__main__':
+    print('Running')
+    run()
